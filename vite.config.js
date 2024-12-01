8faf6d54842e14354681bcf8c9802f69fdd56c01
Varun Singh <iamvarunking619@gmail.com>
2024-12-01 16:40:03 +0530

    change in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/1-pagination/",
+  base:"/pagination-in-react/",
   plugins: [react()],
 })
